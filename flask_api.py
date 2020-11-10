fba64d71669486c954837d588cb4fd0becea514b
lfan6 <lfan6@stevens.edu>
2020-11-09 22:41:41 -0500

    update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -148,7 +148,7 @@ def definition():
 def translate():
     post_json = flask.request.json
     string = post_json.get('string')
-    blob = TextBlob('string')
+    blob = TextBlob(string)
     translation = blob.translate(to='es')
     if string:
         return {'success': True, 'response': translation}
