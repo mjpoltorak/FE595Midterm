8008b945e3e9443b265206f652b2ccc33b127545
Matthew Poltorak <Matthew.Poltorak@verizon.net>
2020-12-09 20:39:17 -0500

    Rewrite API structure, add html help page, and rewrite README

--- README.md
+++ README.md
@@ -1,33 +1,47 @@
 # FE595Midterm
 
-# Sentiment Endpoint (http://ec2-18-191-182-78.us-east-2.compute.amazonaws.com:8080/sentiment)
-    To use this endpoint, send a post request containing json in the format {"string: "your string here"}. The API will return json formatted results with the negative, neutral, positive, and compound sentiment scores.
+# Process String Endpoint
+    http://ec2-18-191-182-78.us-east-2.compute.amazonaws.com:8080/process_string
+    All functions run through the "/process_string" endpoint (a POST) using valid JSON in the format: 
+    {
+        "string": "Test this string for our API",
+        "methods": ["translate", "sentiment"]
+    }
+    where string is a string and methods is a list of strings containing the name of the supported methods (listed below)
 
-# Definition Endpoint (http://ec2-18-191-182-78.us-east-2.compute.amazonaws.com:8080/definition)
-    To use this endpoint, send a post request containing json in the format {"string": "your string here"}. The API will return json formatted results with defitions for each word in the string passed to it.
+# Help Endpoint
+    http://ec2-18-191-182-78.us-east-2.compute.amazonaws.com:8080/help
+    The help endpoint (a GET) explains how to use the API and how to send a post request with valid JSON
+
+
+# Sentiment Function
+    The function will return json formatted results with the negative, neutral, positive, and compound sentiment scores.
+
+# Definition Function 
+    The function will return json formatted results with defitions for each word in the string passed to it.
     
-# Subjectivity Endpoint (http://ec2-18-191-182-78.us-east-2.compute.amazonaws.com:8080/subjectivity)
-    To use this endpoint, send a post request containing json in the format {"string": "your string here"}. The API will return a subjectivity score of the string with 0 being very objective, and 1 being very subjective
+# Subjectivity Function 
+    The function will return a subjectivity score of the string with 0 being very objective, and 1 being very subjective
 
-# Noun Endpoint (http://ec2-18-191-182-78.us-east-2.compute.amazonaws.com:8080/nouns)
-    To use this endpoint, send a post request containing json in the format {"string": "your string here"}. The API will return all the nouns that are a part of the string
+# Noun Function 
+    The function will return all the nouns that are a part of the string
 
-# Verbs Endpoint (http://ec2-18-191-182-78.us-east-2.compute.amazonaws.com:8080/verbs)
-    To use this endpoint, send a post request containing json in the format {"string": "your string here"}. The API will return all instances of verbs (past, present, gerund etc) from the string
+# Verbs Function 
+    The function will return all instances of verbs (past, present, gerund etc) from the string
     
-# Commonwords Endpoint (http://ec2-18-191-182-78.us-east-2.compute.amazonaws.com:8080/commonwords)
-    To use this endpoint, send a post request containing json in the format {"string": "your string here"}. The API will return a dataframe of the 5 most common words using value_counts
+# Commonwords Function 
+    The function will return a dataframe of the 5 most common words using value_counts
 
-# Pluralize Endpoint (http://ec2-18-191-182-78.us-east-2.compute.amazonaws.com:8080/pluralize)
-    To use this endpoint, send a post request containing json in the format {"string": "your string here"}. The API will return the return the plural words associated with each individual token 
+# Pluralize Function 
+    The function will return the return the plural words associated with each individual token 
 
-# Singularize Endpoint (http://ec2-18-191-182-78.us-east-2.compute.amazonaws.com:8080/singularize)
-    To use this endpoint, send a post request containing json in the format {"string": "your string here"}. The API will return all the singular words associated with the individual tokens in the string
+# Singularize Function
+    The function will return all the singular words associated with the individual tokens in the string
     
 
-# Translate Endpoint (http://ec2-18-191-182-78.us-east-2.compute.amazonaws.com:8080/translate)
-    To use this endpoint, send a post request containing json in the format {"string": "your string here"}. The API will return the string, which could be in English or any other recognized language, back in Spanish. 
+# Translate Function 
+    The function will return the string, which could be in English or any other recognized language, back in Spanish. 
 
-# Sentences Endpoint (http://ec2-18-191-182-78.us-east-2.compute.amazonaws.com:8080/sentences)
-    To use this endpoint, send a post request containing json in the format {"string": "your string here"}. The API will return all the sentences in a pandas dataframe ranked from the highest compound sentiment score to the least compound sentiment score of each sentence. 
+# Sentences Function
+    The function will return all the sentences in a pandas dataframe ranked from the highest compound sentiment score to the least compound sentiment score of each sentence. 
 
--- flask_api.py
+++ flask_api.py
@@ -5,104 +5,84 @@ from textblob import Word
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from nltk import tokenize
+import os
 
 app = flask.Flask(__name__)
 
 
-@app.route('/sentiment', methods=['POST'])
-def sentiment():
-    post_json = flask.request.json
-    string = post_json.get('string')
-    if string:
+def sentiment(string):
+    try:
         # calling vader sentiment
         analyzer = SentimentIntensityAnalyzer()
         # returning the negative, neutral, positive, and neutral scores
         polarity = analyzer.polarity_scores(string)
-        return {'success': True, 'response': polarity}
-    else:
-        return {'success': False, 'error': 'No string passed in json payload'}, 400
+        return polarity
+    except:
+        return 'Error occurred calculating sentiment'
 
 
-@app.route('/subjectivity', methods=['POST'])
-def subjectivity():
-    post_json = flask.request.json
-    string = post_json.get('string')
-    if string:
+def subjectivity(string):
+    try:
         blob = TextBlob(string)
         sub = blob.sentiment.subjectivity
-        return {'success': True, 'response': sub}
-    else:
-        return {'success': False, 'error': 'No string passed in json payload'}, 400
+        return sub
+    except:
+        return 'Error occurred calculating subjectivity'
 
 
-@app.route('/commonwords', methods=['POST'])
-def commonwords():
-    post_json = flask.request.json
-    string = post_json.get('string')
-    if string:
+def commonwords(string):
+    try:
         token_list = []
         for w in TextBlob(string).words:
             token_list.append(w)
         tokendf = pd.DataFrame(token_list, columns=['Tokens'])
         mostWords = tokendf['Tokens'].value_counts()[:5]
-        return {'success': True, 'response': mostWords.to_dict()}
-    else:
-        return {'success': False, 'error': 'No string passed in json payload'}, 400
+        return mostWords.to_dict()
+    except:
+        return 'Error occurred calculating common words'
 
 
-@app.route('/nounsverbs', methods=['POST'])
-def nouns():
-    post_json = flask.request.json
-    string = post_json.get('string')
-    if string:
+def nounsverbs(string):
+    try:
         words = nltk.word_tokenize(string)
         tags = nltk.pos_tag(words)
-	verbs = []
+        verbs = []
         nouns = []
-	verb_tag = ['RB','RBR','RBS','VB','VBD','VBG','VBN','VBP','VBZ','WRB']
-	noun_tag = ['NN','NNS','NNP','NNPS','PRP','PRP$','WP','WP$']
+        verb_tag = ['RB','RBR','RBS','VB','VBD','VBG','VBN','VBP','VBZ','WRB']
+        noun_tag = ['NN','NNS','NNP','NNPS','PRP','PRP$','WP','WP$']
         for x in tags:
             if x[1] in verb_tag:
                 verbs.append(x[0])
-	    if x[1] in noun_tag:
-		nouns.append(x[0])
-	dictionary = {"Nouns":nouns, "Verbs":verbs}
-        return {'success': True, 'response': dictionary}
-    else:
-        return {'success': False, 'error': 'No string passed in json payload'}, 400
+            if x[1] in noun_tag:
+                nouns.append(x[0])
+        dictionary = {"Nouns":nouns, "Verbs": verbs}
+        return dictionary
+    except:
+        return 'Error occurred calculating nouns and verbs'
 
 
-@app.route('/pluralize', methods=['POST'])
-def pluralize():
-    post_json = flask.request.json
-    string = post_json.get('string')
-    if string:
+def pluralize(string):
+    try:
         pluralize = ''
         for x in TextBlob(string).words:
             pluralize = pluralize + x.pluralize() + ' '
-        return {'success': True, 'response': pluralize}
-    else:
-        return {'success': False, 'error': 'No string passed in json payload'}, 400
+        return pluralize
+    except:
+        return 'Error occurred pluralizing'
 
 
-@app.route('/singularize', methods=['POST'])
-def singularize():
-    post_json = flask.request.json
-    string = post_json.get('string')
-    if string:
+def singularize(string):
+    try:
         singularize = ''
         for x in TextBlob(string).words:
             singularize = singularize + x.singularize() + ' '
-        return {'success': True, 'response': singularize}
-    else:
-        return {'success': False, 'error': 'No string passed in json payload'}, 400
+        return singularize
+    except:
+        return 'Error occurred calculating singularizing'
 
 
-@app.route('/sentences', methods=['POST'])
-def sentences():
-    post_json = flask.request.json
-    string = post_json.get('string')
-    if string:
+def sentences(string):
+    try:
         # calling vader sentiment
         analyzer = SentimentIntensityAnalyzer()
         # results variable to append the sentences and scores of each sentence
@@ -115,31 +95,26 @@ def sentences():
             results.append([sentence.replace('\n', ' '), score])
         df = pd.DataFrame(results, columns=['Sentence', 'Score']).sort_values('Score', ascending=False).set_index(
             'Sentence')
-        return {'success': True, 'response': df.to_dict()}
-    else:
-        return {'success': False, 'error': 'No string passed in json payload'}, 400
+        return df.to_dict()
+    except:
+        return 'Error occurred finding sentences'
 
 
-@app.route('/definition', methods=['POST'])
-def definition():
-    post_json = flask.request.json
-    string = post_json.get('string')
-    if string:
+def definition(string):
+    try:
         blob = TextBlob(string)
         words = blob.words
         ret_val = []
         for word in words:
             ret_val.append({word: Word.define(word)})
-        return {'success': True, 'response': ret_val}
-    else:
-        return {'success': False, 'error': 'No string passed in json payload'}, 400
+        return ret_val
+    except:
+        return 'Error occurred finding definitions'
 
 
-@app.route('/translate', methods=['POST'])
-def translate():
-    post_json = flask.request.json
-    string = post_json.get('string')
-    new_string = string.replace('\n',' ')
+def translate(string):
+    try:
+        new_string = string.replace('\n', ' ')
         blob = TextBlob(new_string)
         detection = blob.detect_language()
         if detection == 'en':
@@ -149,21 +124,52 @@ def translate():
             chinese = ('Chinese: ' + str(blob.translate(to = 'zh')))
             arabic = ('Arabic: ' + str(blob.translate(to = 'ar')))
             Dictionary = {1: spanish, 2: german, 3: french, 4: chinese, 5: arabic}
-            return {'success': True, 'String is already in English': new_string},'Dictionary below: ', Dictionary
+            return {'detected_language': 'English', 'translations': Dictionary}
         else:
-            list = ['es','fr','de','zh','ar']
-            for lan in list:
-                detection = blob.detect_language()
-                if list.count(detection) == 1:
-                    lang_det = blob.detect_language()
-                    eng = str(blob.translate(to = 'en'))
-                    return {'success': True,'String is in a listed language': new_string, 'Detected Language': lang_det,'English Translation': eng}
-                else: 
-                    lang_det = blob.detect_language()
-                    eng = str(blob.translate(to = 'en'))
-                    return {'success': True,'String is in a non-listed language': new_string, 'Detected Language': lang_det,'English Translation': eng}
+            lang_det = blob.detect_language()
+            eng = str(blob.translate(to = 'en'))
+            return {'detected_language': lang_det, 'translations': {"English": eng}}
+    except:
+        return 'Error occurred translating'
+
+
+@app.route('/help', methods=['GET'])
+def help():
+    return flask.render_template("help.html")
+
+
+@app.route('/process_string', methods=['POST'])
+def process_string():
+    post_json = flask.request.json
+    string = post_json.get('string', None)
+    if string:
+        methods = post_json.get('methods', None)
+        if methods:
+            res_dict = {}
+            if 'sentiment' in methods:
+                res_dict['sentiment'] = sentiment(string)
+            if 'subjectivity' in methods:
+                res_dict['subjectivity'] = subjectivity(string)
+            if 'commonwords' in methods:
+                res_dict['commonwords'] = commonwords(string)
+            if 'nounsverbs' in methods:
+                res_dict['nounsverbs'] = nounsverbs(string)
+            if 'pluralize' in methods:
+                res_dict['pluralize'] = pluralize(string)
+            if 'singularize' in methods:
+                res_dict['singularize'] = singularize(string)
+            if 'sentences' in methods:
+                res_dict['sentences'] = sentences(string)
+            if 'definition' in methods:
+                res_dict['definition'] = definition(string)
+            if 'translate' in methods:
+                res_dict['translate'] = translate(string)
+            return {"success": True, 'response': res_dict}
+        else:
+            return {'success': False, 'error': 'No string passed in json payload'}, 400
     else:
         return {'success': False, 'error': 'No string passed in json payload'}, 400
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
